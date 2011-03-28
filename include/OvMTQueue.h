291ae4294bb50590a41b8aa257670b5c55c5942c
ultrano <hahasasa@nate.com>
2011-03-28 23:46:45 +0900

    메소드, 들여 쓰기 탭 정리

--- include/OvMTQueue.h
+++ include/OvMTQueue.h
@@ -11,22 +11,15 @@ template_class OvMTQueue : public OvMemObject
 	typedef typename OvQueue<T>::reference			reference;
 	typedef typename OvQueue<T>::const_reference	const_reference;
 public:
+	reference			front()			  { OvAutoSection lock(m_cs); return m_queue.front(); };
+	reference			back()			  { OvAutoSection lock(m_cs); return m_queue.back(); };
+	bool				empty()		const { OvAutoSection lock(m_cs); return m_queue.empty(); };
+	size_type			size()		const { OvAutoSection lock(m_cs); return m_queue.size(); };
+	const_reference		front()		const { OvAutoSection lock(m_cs); return m_queue.front(); };
+	const_reference		back()		const { OvAutoSection lock(m_cs); return m_queue.back(); };
 
-	bool empty() const { OvAutoSection lock(m_cs); return m_queue.empty(); };
-
-	size_type size() const{ OvAutoSection lock(m_cs); return m_queue.size(); };
-
-	reference front(){ OvAutoSection lock(m_cs); return m_queue.front(); };
-
-	const_reference front() const{ OvAutoSection lock(m_cs); return m_queue.front(); };
-
-	reference back(){ OvAutoSection lock(m_cs); return m_queue.back(); };
-
-	const_reference back() const{ OvAutoSection lock(m_cs); return m_queue.back(); };
-
-	void push(const value_type& _Val){ OvAutoSection lock(m_cs);  m_queue.push( _Val ); };
-
-	void pop(){ OvAutoSection lock(m_cs);  m_queue.pop(); };
+	void				pop()			  { OvAutoSection lock(m_cs);  m_queue.pop(); };
+	void				push(const value_type& _Val){ OvAutoSection lock(m_cs);  m_queue.push( _Val ); };
 
 private:
 
