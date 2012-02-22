360dc74836e792126cbfc8d79581ebf0d9ea3268
ultrano <hahasasa@nate.com>
2012-02-22 11:40:09 +0900

    컴파일 스캔부분 작업

--- include/MnScriptCompiler.h
+++ include/MnScriptCompiler.h
@@ -1,10 +1,12 @@
 #pragma once
+#include "MnInternal.h"
 
 enum toktype
 {
 	tt_string		= 256,
 	tt_number		= 257,
 	tt_identifier	= 258,
+	tt_eos			= 259,
 };
 struct s_token
 {
@@ -22,72 +24,139 @@ struct s_token
 
 struct compile_state
 {
+	typedef OvSet<OvString> set_str;
+
+	MnState*		s;
+	OvChar			c;
+	OvInputStream*	is;
+
+	set_str			strset;
+
+	s_token*		head;
+	s_token*		itor;
+
 	void		prev() { itor = (itor)? itor->prev : NULL; };
 	void		next() { itor = (itor)? itor->next : NULL; };
-	s_token* head;
-	s_token* itor;
-};
-/*
 
-OvInt cp_token( MnCompileState* cs, OvInt& num, OvString& str )
+};
+
+OvString* cs_new_str( compile_state* cs, OvString& str ) 
 {
-OvChar& c = cs->c;
-num = 0;
-str.clear();
+	compile_state::set_str::iterator itor = cs->strset.find( str );
+	if ( itor == cs->strset.end() )
+	{
+		cs->strset.insert( str );
+		itor = cs->strset.find( str );
+	}
+	return &(*itor);
+}
+
+void cs_push_tok( compile_state* cs, s_token* tok ) 
+{
+	if ( cs->head ) cs->head->prev = tok;
+	tok->next	= cs->head;
+	cs->head	= tok;
+}
+
+void cs_tok_str( compile_state* cs, OvString& str )
+{
+	s_token* tok = new(ut_alloc( sizeof(s_token) )) s_token;
+	tok->type	= tt_string;
+	tok->str	= cs_new_str(cs, str);
+
+	cs_push_tok(cs, tok);
+}
+
+void cs_tok_id( compile_state* cs, OvString& str )
+{
+	s_token* tok = new(ut_alloc( sizeof(s_token) )) s_token;
+	tok->type	= tt_identifier;
+	tok->str	= cs_new_str(cs, str);
+
+	cs_push_tok(cs, tok);
+}
+
+void cs_tok_num( compile_state* cs, OvReal& num )
+{
+	s_token* tok = new(ut_alloc( sizeof(s_token) )) s_token;
+	tok->type	= tt_number;
+	tok->num	= num;
+
+	cs_push_tok(cs, tok);
+}
+
+void cs_tok( compile_state* cs, OvChar c )
+{
+	s_token* tok = new(ut_alloc( sizeof(s_token) )) s_token;
+	tok->type	= c;
+
+	cs_push_tok(cs, tok);
+}
+
+void cs_scan( compile_state* cs )
+{
+	OvChar& c = cs->c;
 #define cp_read() (cs->is->Read(c))
-while ( true )
-{
-if ( c == EOF  )
-{
-return eTEndOfStream;
-}
-else if ( isdigit(c) )
-{
-OvInt mult = 10;
-if ( c == '0' )
-{
-mult = 8;
-cp_read();
-if ( c == 'x' || c == 'X' )
-{
-mult = 16;
-cp_read();
-}
-}
-bool under = false;
-while ( true )
-{
-if ( isdigit(c) ) num = (num * mult) + (c-'0'); else break;
-if ( !cp_read() ) break;
-}
-return eTNumber;
-}
-else if ( c == '"' )
-{
-cp_read();
-do
-{
-str.push_back(c);
-} while ( cp_read() && c != '"' );
-cp_read();
-return eTString;
-}
-else if ( isalpha(c) || c == '_' )
-{
-do
-{
-str.push_back(c);
-} while ( cp_read() && (isalnum(c) || c == '_') );
-return eTIdentifier;
-}
-else
-{
-OvChar ret = c;
-cp_read();
-return ret;
-}
-}
-return eTUnknown;
+
+	while ( true )
+	{
+		if ( c == EOF  )
+		{
+			cs_tok( cs, tt_eos );
+			return ;
+		}
+		else if ( isdigit(c) )
+		{
+			OvReal num = 0;
+			OvInt mult = 10;
+			if ( c == '0' )
+			{
+				mult = 8;
+				cp_read();
+				if ( c == 'x' || c == 'X' )
+				{
+					mult = 16;
+					cp_read();
+				}
+			}
+			bool under = false;
+			while ( true )
+			{
+				if ( isdigit(c) ) num = (num * mult) + (c-'0'); else break;
+				if ( !cp_read() ) break;
+			}
+
+			cs_tok_num( cs, num );
+		}
+		else if ( c == '"' )
+		{
+			OvString str;
+			cp_read();
+			do
+			{
+				str.push_back(c);
+			} while ( cp_read() && c != '"' );
+			cp_read();
+
+			cs_tok_str( cs, str );
+		}
+		else if ( isalpha(c) || c == '_' )
+		{
+			OvString str;
+			do
+			{
+				str.push_back(c);
+			} while ( cp_read() && (isalnum(c) || c == '_') );
+
+			cs_tok_id( cs, str );
+		}
+		else
+		{
+			OvChar ret = c;
+			cp_read();
+
+			cs_tok( cs, ret );
+		}
+	}
 #undef cp_read
 }
-*/
