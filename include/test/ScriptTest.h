4b7bc645108ac466a0338400f972f91be3bb9fc2
ultrano <hahasasa@nate.com>
2012-02-23 10:46:51 +0900

    비트 조절 함수 수정.

--- include/test/ScriptTest.h
+++ include/test/ScriptTest.h
@@ -4,14 +4,3 @@
 #include "OvObject.h"
 
 //////////////////////////////////////////////////////////////////////////
-
-void bit_test()
-{
-	OvInt i = 0;
-	OvInt op,a,b,c;
-	i = cs_code(1,2,3,4);
-	op = cs_getop(i);
-	a = cs_geta(i);
-	b = cs_getb(i);
-	c = cs_getc(i);
-}
