63d714c0f3e30998ede36d9136b75b859bbd0365
hahasasa <hahasasa@nate.com>
2012-04-07 12:37:30 +0900

    스크립트 메이저 번호 상승

--- include/MnInternal.h
+++ include/MnInternal.h
@@ -11,9 +11,9 @@
 #define	MOT_FUNCPROTO	(11)
 #define MOT_TYPEMAX		(12)
 
-#define VERSION_MAJOR	(0)
+#define VERSION_MAJOR	(1)
 #define VERSION_MINOR	(0)
-#define VERSION_PATCH	(3)
+#define VERSION_PATCH	(0)
 
 class MnCallInfo;
 class MnObject;
