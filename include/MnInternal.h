94b392c761048b39fb572d9889795bab7b492612
hahasasa <hahasasa@nate.com>
2012-04-10 01:19:34 +0900

    배열 접근을 zero-base 로 바꿈.

--- include/MnInternal.h
+++ include/MnInternal.h
@@ -986,7 +986,7 @@ void ut_setarray( MnState* s, MnValue& a, MnValue& n, MnValue& v )
 		MnArray* arr = MnToArray(a);
 		if ( MnIsNumber(n) )
 		{
-			MnIndex idx = (MnIndex)MnToNumber(n) - 1;
+			MnIndex idx = (MnIndex)MnToNumber(n);
 			if ( idx >= 0 && idx < arr->array.size() )
 			{
 				arr->array[idx] = v;
@@ -1002,7 +1002,7 @@ MnValue ut_getarray( MnState* s, MnValue& a, MnValue& n )
 		MnArray* arr = MnToArray(a);
 		if ( MnIsNumber(n) )
 		{
-			MnIndex idx = (MnIndex)MnToNumber(n) - 1;
+			MnIndex idx = (MnIndex)MnToNumber(n);
 			if ( idx >= 0 && idx < arr->array.size() )
 			{
 				return arr->array.at(idx);
